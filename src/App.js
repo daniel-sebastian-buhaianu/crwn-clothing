fa79afd212687eb686321086edb38e2beb9c2fc1
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-04-16 10:55:17 +0300

    Create routes for Contact, Shop and SignIn pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import Shop from './routes/shop/shop.component';
+import SignIn from './routes/sign-in/sign-in.component';
+import Contact from './routes/contact/contact.component';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='signin' element={<SignIn />} />
       </Route>
     </Routes>
   );

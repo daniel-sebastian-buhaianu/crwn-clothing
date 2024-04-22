c481f690c095b719b22e861930e8d68edb848803
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-04-22 15:00:07 +0300

    Enable add to cart feature; Display items added to cart inside cart dropdown component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,11 +4,11 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 function CartIcon() {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
   function toggleIsCartOpen() {
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import Button from '../button/button.component';
 import './product-card.styles.scss';
+import { CartContext } from '../../contexts/cart.context';
 
 function ProductCard({ product }) {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`} />
@@ -10,9 +13,14 @@ function ProductCard({ product }) {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType={'inverted'}>Add to cart</Button>
+      <Button buttonType={'inverted'} onClick={addProductToCart}>
+        Add to cart
+      </Button>
     </div>
   );
+  function addProductToCart() {
+    addItemToCart(product);
+  }
 }
 
 export default ProductCard;

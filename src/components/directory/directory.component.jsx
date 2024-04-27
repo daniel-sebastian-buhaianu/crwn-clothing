709bed5ef48ce27b97ec8407b0b171e08d17875a
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-04-27 20:25:53 +0300

    Create Category and CategoriesPreview components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
 import './directory.styles.scss';
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 
 function Directory(props) {
   return (
     <div className='directory-container'>
       {props.categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} directory={category} />
       ))}
     </div>
   );

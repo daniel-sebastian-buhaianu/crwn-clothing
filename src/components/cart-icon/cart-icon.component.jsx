f40953f5112858591b152cc4cfb99463a5726877
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-04-30 16:14:20 +0300

    Refactor CartIcon component using styled component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,18 @@
-import './cart-icon.styles.scss';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
+import {
+  CartIconContainer,
+  ItemCount,
+  ShoppingIcon,
+} from './cart-icon.styles.jsx';
 
 function CartIcon() {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   return (
-    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+    <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{cartCount}</span>
-    </div>
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
   function toggleIsCartOpen() {
     setIsCartOpen(!isCartOpen);

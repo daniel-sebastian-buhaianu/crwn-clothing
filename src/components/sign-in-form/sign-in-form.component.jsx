abe918eab20329654cdad94020d3e7434909ff58
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-05-07 13:06:55 +0300

    Refactor SignInForm component styles using styled-components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import {
 } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-import './sign-in-form.styles.scss';
+import { ButtonsContainer, SignInContainer } from './sign-in-form.styles';
 
 const defaultFormFields = {
   email: '',
@@ -15,8 +15,9 @@ const defaultFormFields = {
 function SignInForm() {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
+
   return (
-    <div className='sign-up-container'>
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleFormSubmit}>
@@ -36,7 +37,7 @@ function SignInForm() {
           name='password'
           value={password}
         />
-        <div className='buttons-container'>
+        <ButtonsContainer>
           <Button type='submit'>Sign In</Button>
           <Button
             type='button'
@@ -45,14 +46,16 @@ function SignInForm() {
           >
             Google Sign In
           </Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignInContainer>
   );
+
   function handleInputChange(event) {
     const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
   }
+
   async function handleFormSubmit(event) {
     event.preventDefault();
     try {
@@ -64,9 +67,11 @@ function SignInForm() {
       console.log(error);
     }
   }
+
   function resetFormFields() {
     setFormFields(defaultFormFields);
   }
+
   async function signInWithGoogle() {
     await signInWithGooglePopup();
     alert('Signed In');

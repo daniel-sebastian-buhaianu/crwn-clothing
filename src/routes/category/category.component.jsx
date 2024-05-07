b6d5390a128fb21c1061d8c7d188ab1cdac69dee
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-05-07 13:41:21 +0300

    Refactor Category component styles using styled-components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,25 +1,27 @@
 import { useParams } from 'react-router-dom';
-import './category.styles.scss';
 import { Fragment, useContext, useEffect, useState } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
+import { CategoryContainer, Title } from './category.styles';
 
 function Category() {
   const { category } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
+
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
+
   return (
     <Fragment>
-      <h1 className='category-title'>{category.toUpperCase()}</h1>
-      <div className='category-container'>
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
+      </CategoryContainer>
     </Fragment>
   );
 }

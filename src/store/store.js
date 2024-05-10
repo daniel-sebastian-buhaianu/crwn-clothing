234a25c89f6fbb2c73fde313ad6c3eb6f5e98214
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-05-10 08:35:11 +0300

    Disable redux-logger in production

--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,9 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [logger];
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean
+);
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
 export const store = createStore(
